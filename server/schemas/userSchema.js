451055cf4065b4c00bba260b99dffd143e891b72
Musa Cekcen <cekcen14@gmail.com>
2025-04-01 16:49:58 +0200

    basic structer updated

--- server/schemas/userSchema.js
+++ server/schemas/userSchema.js
@@ -1,1 +1,49 @@
 import { Sequelize, DataTypes } from "sequelize";
+import { config } from "dotenv";
+
+config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+});
+
+const User = sequelize.define(
+  "User",
+  {
+    user_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 20],
+      },
+      user_email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          len: [8, 100],
+        },
+      },
+    },
+  },
+  {
+    tableName: "user",
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+    underscored: true,
+  }
+);
+
+sequelize.sync();
+
+export default User;
